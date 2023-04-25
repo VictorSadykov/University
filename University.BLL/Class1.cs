77a52d101a7312765b3e2e8e646e7405230617ef
Viktor Sadykov <vityasadykov@gmail.com>
2023-04-25 20:22:06 +0700

    Revert "merge"
    This reverts commit 426b9e09586ecb295bf75299b75602dc32421c6f.

--- University.BLL/Class1.cs
+++ University.BLL/Class1.cs
@@ -1,20 +1,7 @@
-﻿using Newtonsoft.Json;
-
-namespace University.BLL
+﻿namespace University.BLL
 {
-    public class Class1
+   /* public class Class1
     {
-        public void Go()
-        {
-            string text = JsonConvert.SerializeObject()
-
-        }
-    }
-
-    public class ChatData
-    {
-        public long ChatId { get; set; }
-        public string Login { get; set; }
-        public string Password { get; set; }
-    }
+        JsonSe
+    }*/
 }
