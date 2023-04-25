426b9e09586ecb295bf75299b75602dc32421c6f
Viktor Sadykov <vityasadykov@gmail.com>
2023-04-25 20:21:27 +0700

    merge

--- University.BLL/Class1.cs
+++ University.BLL/Class1.cs
@@ -1,7 +1,20 @@
-﻿namespace University.BLL
+﻿using Newtonsoft.Json;
+
+namespace University.BLL
 {
-   /* public class Class1
+    public class Class1
     {
-        JsonSe
-    }*/
+        public void Go()
+        {
+            string text = JsonConvert.SerializeObject()
+
+        }
+    }
+
+    public class ChatData
+    {
+        public long ChatId { get; set; }
+        public string Login { get; set; }
+        public string Password { get; set; }
+    }
 }
