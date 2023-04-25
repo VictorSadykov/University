35b8cd24faf74d1c911d6ace65f3e6952d8188e1
Viktor Sadykov <vityasadykov@gmail.com>
2023-04-25 19:47:39 +0700

    Revert "Revert "Starting configure bot services""
    This reverts commit 88084a56d3a98b800b6c90e93009d6200f218855.

---
+++ University.Bot/BotService.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Telegram.Bot;
+using Telegram.Bot.Exceptions;
+using Telegram.Bot.Types;
+using Telegram.Bot.Polling;
+
+namespace University.Bot
+{
+    public class BotService : BackgroundService
+    {
+        private ITelegramBotClient _telegramClient;
+
+        public BotService(ITelegramBotClient telegramClient)
+        {
+            _telegramClient = telegramClient;
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            _telegramClient.StartReceiving(
+                HandleUpdateAsync,
+                HandleErrorAsync,
+                new ReceiverOptions() { AllowedUpdates = { } },
+                cancellationToken: stoppingToken
+                );
+
+            Console.WriteLine("Бот запущен:");
+        }
+
+        async Task HandleUpdateAsync(ITelegramBotClient botClient, Update update, CancellationToken cancellationToken)
+        {
+            await _telegramClient.SendTextMessageAsync(
+                update.Message.From.Id,
+                update.Message.Text,
+                cancellationToken: cancellationToken
+                );
+        }
+
+        Task HandleErrorAsync(ITelegramBotClient botClient, Exception exception, CancellationToken cancellationToken)
+        {
+            // Задаем сообщение об ошибке в зависимости от того, какая именно ошибка произошла
+            var errorMessage = exception switch
+            {
+                ApiRequestException apiRequestException => $"Telegram API Error:\n[{apiRequestException.ErrorCode}]\n{apiRequestException.Message}",
+                _ => exception.ToString()
+            };
+
+            Console.WriteLine(errorMessage);
+            Console.WriteLine("Ожидаем 10 секунд перед повторным подключением.");
+            Thread.Sleep(10000);
+
+           return Task.CompletedTask;        
+        }
+    }
+}
