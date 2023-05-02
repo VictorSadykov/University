8b159331c010cdc61127bb1d3ae13598eb3d2391
Viktor Sadykov <vityasadykov@gmail.com>
2023-05-02 21:28:38 +0700

    JsonService added

---
+++ University.BLL/JsonService.cs
@@ -0,0 +1,46 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace University.BLL
+{
+    public class JsonService
+    {
+        public void WriteObjectToJson<T>(T objectToSerialize, string path)
+        {
+            string serializedObject = JsonConvert.SerializeObject(objectToSerialize);
+
+            using (StreamWriter sw = new StreamWriter(path))
+            {
+                sw.WriteLine(serializedObject);
+            }
+        }
+
+        public string GetChatDataListInJson()
+        {
+            string output = null;
+
+            using (StreamReader sr = new StreamReader(_chatDataListJsonStoragePath))
+            {
+                output = sr.ReadToEnd();
+            }
+
+            return output;
+        }
+
+        public List<ChatData>? GetChatDataList()
+        {
+            List<ChatData>? chatDataList = JsonConvert
+                .DeserializeObject<List<ChatData>?>(
+                    GetChatDataListInJson()
+                );
+
+            if (chatDataList is null) chatDataList = new List<ChatData>();
+
+            return chatDataList;
+        }
+    }
+}
